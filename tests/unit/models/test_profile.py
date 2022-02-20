1f67de4e6458d944060d45d68fa608a8ecc66087
bloniaq <j.a.blonski@outlook.com>
2022-02-20 15:27:59 +0100

    init file

---
+++ tests/unit/models/test_profile.py
@@ -0,0 +1,47 @@
+import trainer.models.profile as profile
+
+import datetime as dt
+
+
+class TestProfile:
+
+    def test_init(self):
+        assert profile.Profile() is not None
+
+
+class TestPerson:
+
+    def test_init(self):
+        person = profile.Person()
+        assert person is not None
+        assert person.name is ''
+        assert person.birth_date == dt.datetime(1970, 1, 1, 0, 0)
+        assert person.sex is None
+        assert person.height == 0
+
+    def test_set_name(self):
+        person = profile.Person()
+        assert person.name is ''
+        test_name = "Ziutek"
+        person.set_name(test_name)
+        assert person.name == test_name
+
+    def test_set_height(self):
+        person = profile.Person()
+        test_height = 220
+        person.set_height(test_height)
+        assert person.height == test_height
+
+    def test_set_sex(self):
+        person = profile.Person()
+        person.set_sex('male')
+        assert person.sex == 'male'
+        person.set_sex('female')
+        assert person.sex == 'female'
+
+    def test_set_brithdate(self):
+        person = profile.Person()
+        test_bdate = dt.datetime(1990, 8, 28)
+        person.set_birthdate(test_bdate)
+        assert person.birth_date == test_bdate
+        assert person.birth_date == dt.datetime(1990, 8, 28, 0, 0)
