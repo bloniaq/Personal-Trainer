4c8342dddfd805f484dba4342e6b7a5533e6c57c
bloniaq <j.a.blonski@outlook.com>
2022-02-20 15:27:53 +0100

    init file

---
+++ trainer/models/profile.py
@@ -0,0 +1,27 @@
+import datetime as dt
+
+
+class Profile:
+
+    pass
+
+
+class Person:
+
+    def __init__(self):
+        self.name = ''
+        self.birth_date = dt.datetime(1970, 1, 1, 0, 0)
+        self.sex = None
+        self.height = 0
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_height(self, new_height):
+        self.height = new_height
+
+    def set_sex(self, new_sex):
+        self.sex = new_sex
+
+    def set_birthdate(self, new_date):
+        self.birth_date = new_date
